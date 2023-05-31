83befc6eb098e36edfde447f0724cfdd61b136b3
카모뜨린 <55438621+Mascari4615@users.noreply.github.com>
2023-05-31 14:12:26 +0900

    캔버스 매니저, 매니저

---
+++ Assets/Karmotrine/Script/CanvasManager.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    public enum CanvasType
+    {
+        Home,
+        BookShelf,
+        PotionCraft
+    }
+    
+    [SerializeField] private GameObject[] canvasList;
+    private CanvasType curCanvas = CanvasType.Home;
+
+    public void OpenCanvas(int canvasType) => OpenCanvas((CanvasType)canvasType);
+    public void OpenCanvas(CanvasType canvasType)
+    {
+        curCanvas = canvasType;
+
+        for (int i = 0; i < canvasList.Length; i++)
+        {
+            canvasList[i].gameObject.SetActive(i == (int)canvasType);
+        }
+    }
+
+    private void Awake()
+    {
+        OpenCanvas(CanvasType.Home);
+    }
+}
