73f2c5ac6538816d90731a9d075f7f13e1da6d64
Mascari4615 <mascari4615@gmail.com>
2024-03-19 18:19:26 +0900

    Quest가 주도적으로 처리하도록 (QuestManager 삭제)

--- Assets/_Mascari4615/Scripts/Variable/Quest.cs
+++ Assets/_Mascari4615/Scripts/Variable/Quest.cs
@@ -13,20 +13,37 @@ namespace Mascari4615
 	[CreateAssetMenu(fileName = nameof(Quest), menuName = "Variable/" + nameof(Quest))]
 	public class Quest : Artifact
 	{
+		[field: Header("_" + nameof(Quest))]
+		[field: SerializeField] public QuestType QuestType { get; private set; }
 		[field: SerializeField] public GameEvent[] GameEvents { get; private set; }
-		[field: SerializeField] public Criteria[] Criterias{ get; private set; }
-		[field: SerializeField] public QuestType QuestType{ get; private set; }
-		[System.NonSerialized] public bool IsUnlocked;
-		[System.NonSerialized] public bool IsCompleted;
+		[field: SerializeField] public Criteria[] Criterias { get; private set; }
+		[field: System.NonSerialized] public bool IsUnlocked { get; private set; }
+		[field: System.NonSerialized] public bool IsCompleted { get; private set; }
 
 		public void Unlock()
 		{
 			IsUnlocked = true;
+			foreach (GameEvent gameEvent in GameEvents)
+				gameEvent.AddCallback(CheckComplete);
+			CheckComplete();
 		}
 
 		public void Complete()
 		{
 			IsCompleted = true;
+			foreach (GameEvent gameEvent in GameEvents)
+				gameEvent.RemoveCallback(CheckComplete);
+		
+			UIManager.Instance.Popup(this);
+		}
+
+		public void CheckComplete()
+		{
+			foreach (Criteria criteria in Criterias)
+				if (criteria.HasComplete() == false)
+					return;
+
+			Complete();
 		}
 	}
 }
