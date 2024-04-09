17b7cd891411a2581a57d00466eb84de23757e85
Mascari4615 <mascari4615@gmail.com>
2024-04-09 19:54:13 +0900

    새 BGM

--- Assets/_Mascari4615/Scripts/System/AudioManager.cs
+++ Assets/_Mascari4615/Scripts/System/AudioManager.cs
@@ -15,66 +15,62 @@ namespace Mascari4615
 			SFX
 		}
 
-		private readonly Bus[] _buses = new Bus[3];
-		private EventInstance _sfxVolumeTestEvent;
-		private EventInstance _bgmEvent;
-		private PLAYBACK_STATE _pbState;
-		private readonly string[] _bgmTitles = { "Happy" };
-		private int _i = 0;
+		private readonly Bus[] buses = new Bus[3];
+		private EventInstance sfxVolumeTestEvent;
+		private EventInstance bgmEvent;
+		private PLAYBACK_STATE pbState;
+		private readonly string[] bgmTitles = { "PROTODOME" };
+		private int bgmIndex = 0;
 
 		protected override void Awake()
 		{
 			base.Awake();
 
-			_buses[0] = RuntimeManager.GetBus("bus:/");
-			_buses[1] = RuntimeManager.GetBus("bus:/BGM");
-			_buses[2] = RuntimeManager.GetBus("bus:/SFX");
-			_sfxVolumeTestEvent = RuntimeManager.CreateInstance("event:/SFX/SFXTest");
+			buses[0] = RuntimeManager.GetBus("bus:/");
+			buses[1] = RuntimeManager.GetBus("bus:/BGM");
+			buses[2] = RuntimeManager.GetBus("bus:/SFX");
+			sfxVolumeTestEvent = RuntimeManager.CreateInstance("event:/SFX/SFXTest");
 		}
 
 		private void Start()
 		{
 			UpdateVolume();
-			PlayMusic(_bgmTitles[0]);
+			PlayMusic(bgmTitles[0]);
 		}
 
 		private void UpdateVolume()
 		{
-			_buses[0].setVolume(GetVolume(BusType.Master));
-			_buses[1].setVolume(GetVolume(BusType.BGM));
-			_buses[2].setVolume(GetVolume(BusType.SFX));
+			buses[0].setVolume(GetVolume(BusType.Master));
+			buses[1].setVolume(GetVolume(BusType.BGM));
+			buses[2].setVolume(GetVolume(BusType.SFX));
 		}
 
 		private void Update()
 		{
 			// TODO : else if (DataManager.Instance.CurGameData.muteOnOutfocus)
 			{
-				// TODO : 	master.setVolume(0);
+				// TODO : master.setVolume(0);
 			}
-
-			return;
 
 			// if (SceneManager.GetActiveScene().buildIndex == 0)
 			{
-				_bgmEvent.getPlaybackState(out _pbState);
+				bgmEvent.getPlaybackState(out pbState);
 
-				if (_pbState != PLAYBACK_STATE.STOPPED)
+				if (pbState != PLAYBACK_STATE.STOPPED)
 					return;
 
-				_i = (_i + 1) % _bgmTitles.Length;
-
-				_bgmEvent = RuntimeManager.CreateInstance($"event:/BGM/{_bgmTitles[_i]}");
-				_bgmEvent.start();
+				Debug.Log("BGM End");
+				PlayMusic(bgmTitles[bgmIndex = (bgmIndex + 1) % bgmTitles.Length]);
 			}
 		}
 
-		public void StopMusic() => _bgmEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+		public void StopMusic() => bgmEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
 
 		public void PlayMusic(string musicName)
 		{
-			_bgmEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-			_bgmEvent = RuntimeManager.CreateInstance($"event:/BGM/{musicName}");
-			_bgmEvent.start();
+			bgmEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+			bgmEvent = RuntimeManager.CreateInstance($"event:/BGM/{musicName}");
+			bgmEvent.start();
 		}
 
 		public float GetVolume(BusType busType)
@@ -92,13 +88,13 @@ namespace Mascari4615
 		{
 			var key = $"Volume{(int)busType}";
 			PlayerPrefs.SetFloat(key, volume);
-			_buses[(int)busType].setVolume(volume);
+			buses[(int)busType].setVolume(volume);
 
 			if (busType == BusType.SFX)
 			{
-				_sfxVolumeTestEvent.getPlaybackState(out var playbackState);
+				sfxVolumeTestEvent.getPlaybackState(out var playbackState);
 				if (playbackState != PLAYBACK_STATE.PLAYING)
-					_sfxVolumeTestEvent.start();
+					sfxVolumeTestEvent.start();
 			}
 		}
 	}
