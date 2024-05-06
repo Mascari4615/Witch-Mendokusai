fdb7e272a2df9f8a8ff131641dcb81e8befcd24d
Mascari4615 <mascari4615@gmail.com>
2024-05-06 19:44:43 +0900

    간단한 로비 화면 구현

--- Assets/_Mascari4615/Scripts/System/LobbyManager.cs
+++ Assets/_Mascari4615/Scripts/System/LobbyManager.cs
@@ -7,6 +7,8 @@ namespace Mascari4615
 {
 	public class LobbyManager : MonoBehaviour
 	{
+		[SerializeField] private GameObject settingPanel;
+
 		private void Start()
 		{
 			Invoke(nameof(TryLogin), 1f);
@@ -17,9 +19,27 @@ namespace Mascari4615
 			DataManager.Instance.PlayFabManager.Login();
 		}
 
-		public void ForDebug()
+		public void StartGame()
 		{
+			Debug.Log(nameof(StartGame));
 			UISceneLoading.LoadScene("World");
+		}
+
+		public void ExitGame()
+		{
+			Debug.Log(nameof(ExitGame));
+
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+#else
+			Application.Quit();
+#endif
+		}
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Escape))
+				settingPanel.SetActive(false);
 		}
 	}
 }
