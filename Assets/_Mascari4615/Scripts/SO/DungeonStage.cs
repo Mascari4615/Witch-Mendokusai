eb98cf6a0a0b23f00a4128c4a43ea219562f4528
Mascari4615 <mascari4615@gmail.com>
2024-05-08 15:39:53 +0900

    일부 Data 스크립트 경로 변경
    - Variable과 ~Data 구분

--- Assets/_Mascari4615/Scripts/SO/DungeonStage.cs
+++ Assets/_Mascari4615/Scripts/SO/DungeonStage.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Mascari4615
 {
-	[CreateAssetMenu(fileName = "DS_", menuName = "Variable/" + nameof(DungeonStage))]
+	[CreateAssetMenu(fileName = "DS_", menuName = "Data/" + nameof(DungeonStage))]
 	public class DungeonStage : Stage
 	{
 
--- Assets/_Mascari4615/Scripts/SO/WorldStage.cs
+++ Assets/_Mascari4615/Scripts/SO/WorldStage.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Mascari4615
 {
-	[CreateAssetMenu(fileName = "WS_", menuName = "Variable/" + nameof(WorldStage))]
+	[CreateAssetMenu(fileName = "WS_", menuName = "Data/" + nameof(WorldStage))]
 	public class WorldStage : Stage
 	{
 
