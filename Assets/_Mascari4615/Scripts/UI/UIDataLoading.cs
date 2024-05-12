2e0aa2801211ad914c1b3a112aa4ef55e0d7463b
Mascari4615 <mascari4615@gmail.com>
2024-05-13 01:24:48 +0900

    Quest DataSO 경로 수정
    - 기존 QuestData -> Quest 네이밍 변경에 따른,
    - 스크립터블 오브젝트 경로 수정
    - 어드레서블 경로 및 라벨 수정
    
    - 일부 DataSO 스크립터블 오브젝트 이름 수정

--- Assets/_Mascari4615/Scripts/UI/UIDataLoading.cs
+++ Assets/_Mascari4615/Scripts/UI/UIDataLoading.cs
@@ -52,7 +52,7 @@ namespace Mascari4615
 		{
 			SOManager.Instance.DataSOs.Clear();
 
-			LoadAsset<Quest>("QUEST_DATA");
+			LoadAsset<Quest>("QUEST");
 			LoadAsset<CardData>("CARD_DATA");
 			LoadAsset<ItemData>("ITEM_DATA");
 			LoadAsset<MonsterWave>("MONSTER_WAVE");
