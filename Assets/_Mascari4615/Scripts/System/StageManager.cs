a8a9bc41f27fee607f78e6fafafd26317f4a338b
KarmoDDrine <mascari4615@gmail.com>
2025-03-03 14:17:35 +0900

    StageManger 정리
    - 주석 최신화
    - 더 이상 사용되지 않는 코드 삭제 (플레이어 충돌 판정 관련)

--- Assets/_Mascari4615/Scripts/System/StageManager.cs
+++ Assets/_Mascari4615/Scripts/System/StageManager.cs
@@ -8,14 +8,11 @@ namespace Mascari4615
 {
 	public class StageManager : Singleton<StageManager>
 	{
-		private const string NoCollision = "NO_CLLISION";
-		private const string Unit = "Unit";
-
 		[SerializeField] private GameObject homeStageObject;
 		[SerializeField] private Stage homeStage;
 
-		public StageObject CurStageObject { get; private set; }
 		public Stage LastStage { get; private set; }
+		public StageObject CurStageObject { get; private set; }
 		public Stage CurStage { get; private set; }
 
 		private Vector3 lastPosDiff;
@@ -30,37 +27,38 @@ namespace Mascari4615
 		{
 			UIManager.Instance.Transition.Transition(cDuringTransition: LoadStage_(), aWhenEnd: () => UIManager.Instance.StagePopup(stage));
 
+			// 플레이어의 위치는 그대로, 이동할 스테이지가 플레이어 위치에 생성됨
+
+			// isBackToLastStage: 어떠한 이유로 포탈 타기 전 마지막 위치로 돌아간다는 뜻.
+			// i.e. 던전에서 나갈 때 던전을 진입했던 위치로 돌아감 (이렇게 포탈을 타는 것이 아닌, 특정한 이유로 원래 스테이지로 돌아가는 경우)
+			// i.e. 포탈을 타고 A -> B 스테이지로 왔다가 다시 B -> A로 돌아가는 경우는 해당 사항이 아님.
+
+			// 주석 참고:
+			// 함수 시작: 이전 스테이지 Z -> 현재 스테이지 A -> 이동할 스테이지 B
+			// 함수 종료: 이전 스테이지 A -> 현재 스테이지 B (스테이지 Z는 사용되지 않음.)
 			IEnumerator LoadStage_()
 			{
-				// 플레이어는 가만히 있고, 스테이지가 위치에 따라 생성됨
-				// SpawnPortalIndex가 -1이면, 포탈 타기 전 마지막 위치로 돌아간다는 뜻
-
-				// https://gall.dcinside.com/mgallery/board/view/?id=game_dev&no=99368
-				// 플레이어가 더 이상 충돌로 Interactive 오브젝트들을 찾지 않음
-				// PlayerController.Instance.SetInteractionColliderLayer(LayerMask.NameToLayer(NoCollision));
-
-				// TimeScale 0이면 FixedUpdate가 호출되지 않기 때문에 임의로 조정
 				TimeManager.Instance.Resume();
 				yield return new WaitForFixedUpdate();
 				yield return new WaitForFixedUpdate();
 				yield return new WaitForFixedUpdate();
 
-				// 마지막 스테이지를 현재 스테이지로 갱신하고, 비활성화 
 				Vector3 newLastPosDiff = Player.Instance.transform.position - CurStageObject.gameObject.transform.position;
-				// Debug.Log(newLastPosDiff);
+
+				// LastStage를 A로 갱신하고, 비활성화
 				CurStageObject.gameObject.SetActive(false);
 				LastStage = CurStage;
 
-				// 새로운 스테이지 생성 (비활성화 상태)
+				// 새로운 스테이지 B 생성 (비활성화 상태)
 				GameObject targetStage = stage.Prefab.gameObject;
 				CurStageObject = ObjectPoolManager.Instance.Spawn(targetStage).GetComponent<StageObject>();
 
-				// 새로운 스테이지 위치 변환
-				// TODO: 
+				// 새로운 스테이지 B 위치 변환
+				// TODO:
 				// Vector3 portalTPPos = stage.Prefab.Portals[spawnPortalIndex].TpPos.position;
 				Vector3 newStagePos;
 				{
-					if (isBackToLastStage)
+					if (isBackToLastStage == true)
 					{
 						newStagePos = Player.Instance.transform.position - lastPosDiff;
 					}
@@ -80,17 +78,16 @@ namespace Mascari4615
 				}
 				CurStageObject.transform.position = newStagePos;
 
-				// 새로운 스테이지 활성화
+				// 새로운 스테이지 B 활성화, CurStage를 B로 갱신
 				CurStageObject.gameObject.SetActive(true);
 				CurStage = stage;
 
+				// 마지막 이동 위치 갱신 (B -> A로 다시 되돌아가는 경우)
 				lastPosDiff = newLastPosDiff;
 
 				yield return new WaitForFixedUpdate();
 				yield return new WaitForFixedUpdate();
 				yield return new WaitForFixedUpdate();
-				// 플레이어가 더 이상 충돌로 Interactive 오브젝트들을 찾지 않음
-				// PlayerController.Instance.SetInteractionColliderLayer(LayerMask.NameToLayer(Unit));
 				TimeManager.Instance.Pause();
 
 				action?.Invoke();
